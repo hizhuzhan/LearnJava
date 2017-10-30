8a3c00cc3270fddbbb1e2311d20f217ce3ce02d6
Zhan Zhu <hizhuzhan@outlook.com>
2017-10-30 17:26:36 +0800

    Update BaiYuanBaiJi
    百元百鸡

---
+++ learnJava/src/bai_yuan_bai_chicken/test.java
@@ -0,0 +1,14 @@
+package bai_yuan_bai_chicken;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x = 9;
+		System.out.println(x/3.0);
+
+	}
+
+}
