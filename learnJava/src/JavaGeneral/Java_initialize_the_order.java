27175637fdc4ccafa5ba5ef7bbfd8ef6b3b5ce5c
Zhan Zhu <hizhuzhan@outlook.com>
2017-11-09 13:51:13 +0800

    update

--- learnJava/src/JavaGeneral/Java_initialize_the_order.java
+++ learnJava/src/JavaGeneral/Java_initialize_the_order.java
@@ -15,32 +15,64 @@ package JavaGeneral;
  * 
  */
 
-class Base{
+//class Base{
+//	static{
+//		System.out.println("Base static block");
+//	}
+//	{
+//		System.out.println("Base block");
+//	}
+//	public Base(){
+//		System.out.println("Base constructor");
+//	}
+//	static{
+//		System.out.println("static Base static block");
+//	}
+//}
+//
+//
+//public class Java_initialize_the_order extends Base{
+//	
+//	static{
+//		System.out.println("Derived static block");
+//	}
+//	{
+//		System.out.println("Derived block");
+//	}
+//	public Java_initialize_the_order(){
+//		System.out.println("Derived constructor");
+//	}
+//	public static void main(String[] args){
+//		new Java_initialize_the_order();
+//	}	
+//}
+
+
+class B extends Object{
 	static{
-		System.out.println("Base static block");
+		System.out.println("Load B1");
 	}
-	{
-		System.out.println("Base block");
+	public B(){
+		System.out.println("Create B3");
 	}
-	public Base(){
-		System.out.println("Base constructor");
+	static{
+		System.out.println("Load B2");
+	}
+}
+class A extends B{
+	static{
+		System.out.println("Load B4");
+	}
+	public A(){
+		System.out.println("Creade A");
+	}
+	static{
+		System.out.println("Creade A");
 	}
 }
 
-
-public class Java_initialize_the_order extends Base{
-	
-	static{
-		System.out.println("Derived static block");
-	}
-	{
-		System.out.println("Derived block");
-	}
-	public Java_initialize_the_order(){
-		System.out.println("Derived constructor");
-	}
+public class Java_initialize_the_order{
 	public static void main(String[] args){
-		new Java_initialize_the_order();
+		new A();
 	}
-		
 }
